5e91cc1cb1e1cd6cd68360b86cbfea7eaf65b224
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2026-01-17 22:39:47 +0200

    Add keycloak to the project, authenticate and create a new realm

---
+++ dotnet/keycloak/keycloak.AppHost/Keycloak/IKeycloakAgent.cs
@@ -0,0 +1,19 @@
+﻿using keycloak.AppHost.Keycloak.Model;
+using Refit;
+
+namespace keycloak.AppHost.Keycloak
+{
+    interface IKeycloakAgent
+    {
+        [Post("/realms/{realm}/protocol/openid-connect/token")]
+        Task<TokenResponse> GetToken(string realm, [Body(BodySerializationMethod.UrlEncoded)] PasswordGrantRequest request);
+
+        [Get("/admin/realms")]
+        Task<List<KeycloackRealm>> GetRealmInformation();
+
+        [Post("/admin/realms")]
+        Task AddRealm([Body] RealmRequest request);
+    }
+
+
+}
